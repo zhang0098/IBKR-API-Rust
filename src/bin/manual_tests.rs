a38715d7bf270954c6170f8e913ff832373d2b0a
zhang <zhangbaohe@gmail.com>
2023-02-10 15:17:49 +0800

    commit

--- src/bin/manual_tests.rs
+++ src/bin/manual_tests.rs
@@ -1,13 +1,20 @@
 //! Binary for manually testing crate
 
 use log::*;
-use simple_logging;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use twsapi::core::client::EClient;
 use twsapi::core::{errors::*, streamer::TcpStreamer};
 use twsapi::examples::test_helpers::TestWrapper;
+
+use chrono::Local;
+use env_logger;
+use env_logger::Builder;
+use log::LevelFilter;
+use std::file;
+use std::fs::File;
+use std::io::Write;
 /// Example of using client and wrapper.
 /// Requires a running instance of TWS or IB Gateway connected to the port in main.
 /// Upon connecting, TWS will send the next valid order ID which will cause the wrapper callback method
@@ -15,7 +22,22 @@ use twsapi::examples::test_helpers::TestWrapper;
 /// start_requests function inn TestWrapper which is called by next_valid_id
 //==================================================================================================
 pub fn main() -> Result<(), IBKRApiLibError> {
-    _ = simple_logging::log_to_file("log/output.log", LevelFilter::Info);
+    let target = Box::new(File::create("./output.log").expect("Can't create file"));
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(
+                buf,
+                "{}:{} {} [{}] - {}",
+                record.file().unwrap_or("unknown"),
+                record.line().unwrap_or(0),
+                Local::now().format("%Y-%m-%dT%H:%M:%S%.3f"),
+                record.level(),
+                record.args()
+            )
+        })
+        .target(env_logger::Target::Pipe(target))
+        .filter(None, LevelFilter::Info)
+        .init();
 
     let wrapper = Arc::new(Mutex::new(TestWrapper::<TcpStreamer>::new()));
     let app = Arc::new(Mutex::new(EClient::new(wrapper.clone())));
