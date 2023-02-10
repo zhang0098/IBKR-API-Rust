f07eaf6e5642e849c1377219b3a4ca222ecc1709
zhang <zhangbaohe@gmail.com>
2023-02-10 14:53:14 +0800

    commit

--- src/bin/manual_tests.rs
+++ src/bin/manual_tests.rs
@@ -1,13 +1,13 @@
 //! Binary for manually testing crate
 
 use log::*;
+use simple_logging;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 use twsapi::core::client::EClient;
 use twsapi::core::{errors::*, streamer::TcpStreamer};
 use twsapi::examples::test_helpers::TestWrapper;
-
 /// Example of using client and wrapper.
 /// Requires a running instance of TWS or IB Gateway connected to the port in main.
 /// Upon connecting, TWS will send the next valid order ID which will cause the wrapper callback method
@@ -15,16 +15,7 @@ use twsapi::examples::test_helpers::TestWrapper;
 /// start_requests function inn TestWrapper which is called by next_valid_id
 //==================================================================================================
 pub fn main() -> Result<(), IBKRApiLibError> {
-    match log4rs::init_file("./log_config.yml", Default::default()) {
-        Ok(_) => (),
-        Err(_) => {
-            return Err(IBKRApiLibError::ApiError(TwsApiReportableError::new(
-                -1,
-                "-1".to_string(),
-                "Failed to create logger!!".to_string(),
-            )))
-        }
-    };
+    _ = simple_logging::log_to_file("log/output.log", LevelFilter::Info);
 
     let wrapper = Arc::new(Mutex::new(TestWrapper::<TcpStreamer>::new()));
     let app = Arc::new(Mutex::new(EClient::new(wrapper.clone())));
