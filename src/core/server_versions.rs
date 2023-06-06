eb910292221e6215da8751787cb7274ecda83519
ZHANG <zhangbaohe@gmail.com>
2023-06-06 17:51:34 +0800

    misc

--- src/core/server_versions.rs
+++ src/core/server_versions.rs
@@ -97,4 +97,4 @@ pub const MIN_SERVER_VER_PRICE_MGMT_ALGO: i32 = 151;
 // 100 = enhanced handshake, msg length prefixes
 
 pub const MIN_CLIENT_VER: i32 = 100;
-pub const MAX_CLIENT_VER: i32 = MIN_SERVER_VER_PRICE_MGMT_ALGO;
+pub const MAX_CLIENT_VER: i32 = 163;
